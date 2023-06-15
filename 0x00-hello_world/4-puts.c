a372ac59674fd5625d60a692060e10c9325bb6e1
seyjay1 <seyramagawuga12@gmail.com>
2023-06-15 15:32:37 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : using puts function
+ * Return: End program
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
